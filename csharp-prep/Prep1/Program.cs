8d72e9ea7ee1348be1a4dc74c0047875c7272599
Nathan Baldauf <baldaufnathan@gmail.com>
2024-04-25 20:46:45 -0600

    Wrote the Bond, James Bond code

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first name? ");
+        string nb_first = Console.ReadLine();
+        Console.WriteLine("What is your last name? ");
+        string nb_last = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {nb_last}, {nb_first} {nb_last}");
     }
 }
